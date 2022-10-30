73a02cabbcef868cd10bff7c64c1fde8f7000e4e
fishtriangle <66234622+fishtriangle@users.noreply.github.com>
2022-10-30 21:00:43 +1200

    add express middleware

--- src/server.js
+++ src/server.js
@@ -1,23 +1,46 @@
 import { ApolloServer } from '@apollo/server';
 import schema from './schema.js';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import context from './context.js';
 import process from 'node:process';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const { typeDefs, resolvers } = schema;
-// const server = new ApolloServer({ typeDefs, resolvers, context });
 
-// export const outputServer = () => {
-//   server.listen().then(({ url }) => console.log(`Server listen at: ${url}`));
+// const outputServer = async () => {
+//   const server = new ApolloServer({ typeDefs, resolvers });
+//   const { url } = await startStandaloneServer(server, {
+//     context: () => context,
+//     listen: { port: process.env.PORT || 4000 },
+//   });
+//   console.log(`🚀  Server ready at ${url}`);
 // };
 
 const outputServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  const { url } = await startStandaloneServer(server, {
-    context: () => context,
-    listen: { port: process.env.PORT || 4000 },
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  console.log(`🚀  Server ready at ${url}`);
+  await server.start();
+  app.use(
+    '/',
+    cors(),
+    bodyParser.json({ limit: '50mb' }),
+    expressMiddleware(server, {
+      context: () => context,
+    })
+  );
+  await new Promise((resolve) =>
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+  );
+  console.log(`Server ready on port ${process.env.PORT || 4000}`);
 };
 
 export default outputServer;
